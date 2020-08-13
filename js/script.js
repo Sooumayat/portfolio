738b5877a47d79c591abf33ebafc4896ab77f38e
Sooumayat <soumayat.hamadi@hotmail.com>
2020-08-13 11:39:27 +0200

    suppresion du fichier js

--- js/script.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $(".navbar a, footer a").on("click", function(event){
-      event.preventDefault();
-      var hash = this.hash;
-
-      $('body').animate([scrollTop: $(hash).offset().top] , 980, function(){window.location.hash = hash;})
-    });
-})
